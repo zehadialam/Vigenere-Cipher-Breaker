978704502637becbb56605f7b91bc4676902cdc3
Zehadi Alam <63765084+zehadialam@users.noreply.github.com>
2021-05-25 12:18:48 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <map>
-#include <algorithm>
+//#include <algorithm>
 #include <valarray>
 
 #include "nGramScorer.h"
@@ -40,58 +40,66 @@ string trigrams(int n, char arr[]) {
     return trigram;
 }
 
+string formatCiphertext(const string& ciphertext) {
+    string formattedCiphertext{};
+    for (char c : ciphertext) {
+        if (isalpha(c)) {
+            formattedCiphertext += toupper(c);
+        }
+    }
+    return formattedCiphertext;
+}
+
+char * alphabet() {
+    char arr[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                  'U', 'V', 'W', 'X', 'Y', 'Z'};
+    return arr;
+}
+
 int main() {
     nGramScorer qgram(ifstream(R"(C:\Users\zna58005\CLionProjects\vigenerecipher\quadgrams.txt)"));
     nGramScorer trigram(ifstream(R"(C:\Users\zna58005\CLionProjects\vigenerecipher\trigrams.txt)"));
-    string s = "Slycmiskhtvp, mh wrlinaevkm, zq jbe ckuompgs rlt mthws ay aiqylykurl zak ziqlpu wozfozbjehzmd cn gay bklwseau if gacdw wefkgum";
-    string s2 = {};
-    for (char c : s) {
-        if (isalpha(c)) {
-            s2 += toupper(c);
-        }
-    }
-    //cout << s2 << endl;
+    string ciphertext = formatCiphertext("Slycmiskhtvp, mh wrlinaevkm, zq jbe ckuompgs rlt mthws ay aiqylykurl zak ziqlpu wozfozbjehzmd cn gay bklwseau if gacdw wefkgum");
+    //cout << ciphertext << endl;
     cout << setprecision(16) << qgram.score(
             "CRYPTOGRAPHYORCRYPTOLOGYISTHEPRACTICEANDSTUDYOFTECHNIQUESFORSECURECOMMUNICATIONINTHEPRESENCEOFTHIRDPARTIES")
          << endl;
     char arr[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
                   'U', 'V', 'W', 'X', 'Y', 'Z'};
     string A(13 - 3, 'A');
-    map<double, string> firstThree;
+    map<double, string> keyCandidates;
     for (int i = 0; i < (int) pow(26, 3); i++) {
         string key = trigrams(i, arr) + A;
-        string key2 = generateKey(s2, key);
-        string pt = originalText(s2, key2);
+        string key2 = generateKey(ciphertext, key);
+        string pt = originalText(ciphertext, key2);
         double score = 0;
-        for (int j = 0; j < s2.length(); j += 13) { // 13 is the key length
+        for (int j = 0; j < ciphertext.length(); j += 13) { // 13 is the keyBuilder length
             score += trigram.score(pt.substr(j, 3));
         }
-        firstThree[score] = key.substr(0, 3);
+        keyCandidates[score] = key.substr(0, 3);
     }
-    string partialKey = firstThree[firstThree.rbegin()->first];
-    cout << partialKey << endl;
-    string key[] = {partialKey};
-    for (int i = 0; i < 10; i++) {
+    string partialKey = keyCandidates[keyCandidates.rbegin()->first];
+    //cout << partialKey << endl;
+    string keyBuilder[] = {partialKey};
+    for (int i = 0; i < 10; i++) { // keyBuilder length - 3
+        keyCandidates.clear();
         for (char c : arr) {
-            firstThree.clear();
-            partialKey = key[0] + c;
+            partialKey = keyBuilder[0] + c;
             string pad(13 - partialKey.length(), 'A');
             string fullKey = partialKey + pad;
-            string fullKey2 = generateKey(s2, fullKey);
-            string pt = originalText(s2, fullKey2);
+            string fullKey2 = generateKey(ciphertext, fullKey);
+            string pt = originalText(ciphertext, fullKey2);
             double score = 0;
-            for (int j = 0; j < s2.length(); j += 13) {
+            for (int j = 0; j < ciphertext.length(); j += 13) {
                 if (j + partialKey.length() < pt.length()) {
                     score += qgram.score(pt.substr(j, partialKey.length()));
                 }
             }
-            cout << partialKey << " " << score << endl;
-            //firstThree[score] = fullKey.substr(0, partialKey.length());
-            //partialKey = firstThree[firstThree.rbegin()->first];
+            keyCandidates[score] = partialKey;
         }
-
+        //cout << keyCandidates[keyCandidates.rbegin()->first] << endl;
+        keyBuilder[0] = keyCandidates[keyCandidates.rbegin()->first];
     }
-    partialKey = firstThree[firstThree.rbegin()->first];
-    cout << partialKey << endl;
+    cout << keyCandidates[keyCandidates.rbegin()->first] << endl;
     return EXIT_SUCCESS;
 }
