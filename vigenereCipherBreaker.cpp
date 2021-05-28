3971a539d4c1ace998432316af634c8fe53b8ead
Zehadi Alam <63765084+zehadialam@users.noreply.github.com>
2021-05-27 21:10:52 -0400

    Add files via upload

--- vigenereCipherBreaker.cpp
+++ vigenereCipherBreaker.cpp
@@ -1,3 +1,5 @@
+#include <chrono>
+#include <cstring>
 #include <iomanip>
 #include <iostream>
 #include <unordered_map>
@@ -6,7 +8,9 @@
 #include "nGramScorer.h"
 #include "vigenereCipher.h"
 
-using namespace std;
+using std::string;
+using std::cout;
+using std::endl;
 
 string theAlphabet() {
     return "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -22,6 +26,24 @@ string formatCiphertext(const string &ciphertext) {
     return formattedCiphertext;
 }
 
+string restoreOriginalFormat(const string &originalFormat, string modifiedFormat) {
+    string restoredText{};
+    int j = 0;
+    for (char i : originalFormat) {
+        if (isalpha(i)) {
+            if (isupper(i)) {
+                restoredText += toupper(modifiedFormat[j]);
+            } else if (islower(i)) {
+                restoredText += tolower(modifiedFormat[j]);
+            }
+            j++;
+        } else {
+            restoredText += i;
+        }
+    }
+    return restoredText;
+}
+
 string trigramPermutations(int n, string alphabet) {
     string trigram{};
     for (int i = 0; i < 3; i++) {
@@ -32,7 +54,7 @@ string trigramPermutations(int n, string alphabet) {
 }
 
 string firstThreeKeyLetters(nGramScorer trigram, const string &alphabet, const string &ciphertext, int keyLength) {
-    map<double, string> keyCandidates;
+    std::map<double, string> keyCandidates;
     string pad(keyLength - 3, 'A');
     for (int i = 0; i < (int) pow(26, 3); i++) {
         string key = trigramPermutations(i, alphabet) + pad;
@@ -51,7 +73,7 @@ string firstThreeKeyLetters(nGramScorer trigram, const string &alphabet, const s
 
 string
 fullKey(nGramScorer quadgram, const string &alphabet, const string &ciphertext, int keyLength, string keyBuilder) {
-    map<double, string> keyCandidates;
+    std::map<double, string> keyCandidates;
     for (int i = 0; i < keyLength - 3; i++) {
         keyCandidates.clear();
         for (char c : alphabet) {
@@ -74,24 +96,52 @@ fullKey(nGramScorer quadgram, const string &alphabet, const string &ciphertext, 
     return keyCandidates[keyCandidates.rbegin()->first];
 }
 
-int main() {
-    nGramScorer trigram(ifstream(R"(..\ngrams\trigrams.txt)"));
-    nGramScorer quadgram(ifstream(R"(..\ngrams\quadgrams.txt)"));
-    string ciphertext = formatCiphertext(
-            "Uvagxhvrshdm, fu uvagxhaoyq, eg kkw ttrgmxcw sjr jwmha fj mtczfeelhk jqi wxrujw ycdpmrktemxof aj hyh hvgjigre gx pvzuv tcixbts.");
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+        std::cerr << "Error: invalid number of command line arguments. Please use the following syntax:" << endl;
+        std::cerr << "./vigenereCipherBreaker ciphertext min keylength max keylength verbose mode" << endl;
+        exit(EXIT_FAILURE);
+    }
+    nGramScorer trigram(std::ifstream(R"(..\ngrams\trigrams.txt)"));
+    nGramScorer quadgram(std::ifstream(R"(..\ngrams\quadgrams.txt)"));
+    string originalCipherText = argv[1];
+    string formattedCipherText = formatCiphertext(originalCipherText);
     string alphabet = theAlphabet();
-    int rangeStart = 4;
-    int rangeEnd = 20;
+    std::map<double, string> keyCandidates;
+    int rangeStart = std::stoi(argv[2]);
+    int rangeEnd = std::stoi(argv[3]);
+    bool verboseMode = strcmp(argv[4], "0");
+    cout << "Estimated time to break the encryption and unlock the message: " << ceil(0.33 * (rangeEnd - rangeStart))
+         << " seconds\n" << endl;
+    auto startTime = std::chrono::high_resolution_clock::now();
     for (int i = rangeStart; i < rangeEnd; i++) {
         int keyLength = i; // Need to fix keylength. Only works with length greater than 4.
-        string keyBuilder = firstThreeKeyLetters(trigram, alphabet, ciphertext, keyLength);
-        string theKey = fullKey(quadgram, alphabet, ciphertext, keyLength, keyBuilder);
+        string keyBuilder = firstThreeKeyLetters(trigram, alphabet, formattedCipherText, keyLength);
+        string theKey = fullKey(quadgram, alphabet, formattedCipherText, keyLength, keyBuilder);
         double bestScore = quadgram.score(
-                vigenereCipher::decrypt(ciphertext, vigenereCipher::formatKey(ciphertext, theKey)));
-        cout << "Score: " << setprecision(16) << bestScore << ", " << "Key length: " << keyLength << ", " << "Key: "
-             << theKey << "\n" << "Decrypted: "
-             << vigenereCipher::decrypt(ciphertext, vigenereCipher::formatKey(ciphertext, theKey))
+                vigenereCipher::decrypt(formattedCipherText, vigenereCipher::formatKey(formattedCipherText, theKey)));
+        keyCandidates[bestScore] = theKey;
+        if (verboseMode) {
+            cout << "Score: " << std::setprecision(16) << bestScore << ", " << "Key length: " << keyLength << ", "
+                 << "Key: "
+                 << theKey << "\n" << "Decrypted: "
+                 << vigenereCipher::decrypt(formattedCipherText, vigenereCipher::formatKey(formattedCipherText, theKey))
+                 << "\n" << endl;
+        }
+    }
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    auto timeTaken = elapsedTime.count() * 0.001;
+    if (!verboseMode) {
+        cout << "POTENTIAL MATCH FOUND\n" << endl;
+        cout << "KEY: " << keyCandidates[keyCandidates.rbegin()->first] << "\n" << endl;
+        cout << "DECRYPTED MESSAGE:\n" << endl;
+        cout << restoreOriginalFormat(originalCipherText, vigenereCipher::decrypt(formattedCipherText,
+                                                                                  vigenereCipher::formatKey(
+                                                                                          formattedCipherText,
+                                                                                          keyCandidates[keyCandidates.rbegin()->first])))
              << "\n" << endl;
     }
+    printf("Total elapsed time: %.2f seconds\n", timeTaken);
     return EXIT_SUCCESS;
 }
