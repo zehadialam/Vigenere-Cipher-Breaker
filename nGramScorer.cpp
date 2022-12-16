68ef43c0b549294855349ea807441e092c586315
Zehadi Alam <63765084+zehadialam@users.noreply.github.com>
2022-12-15 21:56:23 -0500

    Optimized score function using pointers

--- nGramScorer.cpp
+++ nGramScorer.cpp
@@ -8,12 +8,14 @@ nGramScorer::nGramScorer(std::ifstream file) {
     if (file.is_open()) {
         nGramFrequencies.reserve(389373); // Pre-allocate space for the n-gram frequencies map.
         while (getline(file, line)) {
+            // Extract the n-gram and its total occurrence from the line
             std::stringstream lineStream(line);
             std::string ngram;
             int nGramTotalOccurrence;
             lineStream >> ngram >> nGramTotalOccurrence;
             nGramLength = (int) ngram.length();
             sumTotalOccurrence += nGramTotalOccurrence;
+            // Store the n-gram frequency in the map
             nGramFrequencies[ngram] = nGramTotalOccurrence;
         }
         file.close();
@@ -27,12 +29,19 @@ nGramScorer::nGramScorer(std::ifstream file) {
 double nGramScorer::score(const std::string &text) {
     double score = 0;
     double floor = log10(0.01) - log10(sumTotalOccurrence);
-    int textLength = text.length();
-    for (int i = 0; i < textLength - nGramLength + 1; i++) {
-        const std::string &nGram = text.substr(i, nGramLength);
-        if (nGramFrequencies.count(nGram)) {
-            score += nGramFrequencies[nGram];
+    // Get pointers to the start and end of the text
+    const char* textStart = text.data();
+    const char* textEnd = textStart + text.size();
+    // Iterate through each n-gram in the text
+    // Continue until current character plus the n-gram length is <= the end of the text.
+    for (const char* currentChar = textStart; currentChar <= textEnd - nGramLength; currentChar++) {
+        // Look up the n-gram frequency in the map
+        auto it = nGramFrequencies.find(std::string(currentChar, currentChar + nGramLength));
+        if (it != nGramFrequencies.end()){
+            // If the n-gram is found, add its frequency to the score
+            score += it->second;
         } else {
+            // If the n-gram is not found, add the floor value to the score
             score += floor;
         }
     }
