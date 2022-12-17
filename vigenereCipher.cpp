9cf2c339041dac69ee9fd32ecb30aa11006652da
Zehadi Alam <63765084+zehadialam@users.noreply.github.com>
2022-12-16 20:01:41 -0500

    Update code documentation

--- vigenereCipher.cpp
+++ vigenereCipher.cpp
@@ -1,37 +1,68 @@
+/**
+ * @author Zehadi Alam
+ */
+ 
 #include "vigenereCipher.h"
 
+/**
+ * Formats the key string so that it is at least as long as the ciphertext string.
+ *
+ * @param ciphertext The ciphertext string.
+ * @param key The key string.
+ * @return The formatted key string.
+ */
 std::string vigenereCipher::formatKey(const std::string &ciphertext, const std::string &key) {
     std::string cipherTextLengthKey{};
-    for (int i = 0; i < (int) ciphertext.length(); i += (int) key.length()) {
+    int ciphertextLength = ciphertext.length();
+    int keyLength = key.length();
+    // Repeatedly append the key to the cipherTextLengthKey string until it is at least 
+    // as long as the ciphertext
+    for (int i = 0; i < ciphertextLength; i += keyLength) {
         cipherTextLengthKey += key;
     }
+    // Return a substring of the formatted key that is the same length as the ciphertext
     return cipherTextLengthKey.substr(0, ciphertext.length());
 }
 
+/**
+ * Decrypts a ciphertext string using the Vigenère cipher with the given key.
+ *
+ * @param ciphertext The ciphertext string to decrypt.
+ * @param key The key to use for decryption.
+ * @return The decrypted plaintext string.
+ */
 std::string vigenereCipher::decrypt(std::string ciphertext, std::string key) {
     std::string decrypted{};
-    int ciphertextLength = ciphertext.length();
+	int ciphertextLength = ciphertext.length();
     for (int i = 0; i < ciphertextLength; i++) {
-        // Get the current ciphertext and key characters
-        char c = ciphertext[i];
-        char k = key[i];
-        int decryptedChar = ((c - 65) - (k - 65)) % 26; // Decrypt the ciphertext character
-        if (decryptedChar < 0) { // handle negative values
-            decryptedChar += 26;
-        }
-        // Convert the decrypted character back to uppercase and add it to the decrypted message
-        char decryptedCharUpper = decryptedChar + 65;
-        decrypted += decryptedCharUpper;
+		// Get the current ciphertext and key characters
+		char c = ciphertext[i];
+		char k = key[i];
+		int decryptedChar = ((c - 65) - (k - 65)) % 26; // Decrypt the ciphertext character
+		if (decryptedChar < 0) { // handle negative values
+			decryptedChar += 26;
+		}
+		// Convert the decrypted character back to uppercase and add it to the decrypted message
+		char decryptedCharUpper = decryptedChar + 65;
+		decrypted += decryptedCharUpper;
     }
     return decrypted;
 }
 
+/**
+ * Sets the length of the key used for encryption and decryption.
+ *
+ * @param length The length of the key.
+ */
 void vigenereCipher::setKeyLength(int length) {
     vigenereCipher::keyLength = length;
 }
 
+/**
+ * Gets the length of the key used for encryption and decryption.
+ *
+ * @return The length of the key.
+ */
 int vigenereCipher::getKeyLength() {
     return vigenereCipher::keyLength;
 }
-
-
--- vigenereCipher.h
+++ vigenereCipher.h
@@ -1,19 +1,62 @@
-#include <string>
+/**
+ * @file vigenere_cipher.h
+ *
+ * @brief Header file for the Vigenère cipher.
+ *
+ * @author Zehadi Alam
+ */
 
 #ifndef VIGENERECIPHER_H
 #define VIGENERECIPHER_H
 
+#include <string>
+
+/**
+ * @class vigenereCipher
+ *
+ * @brief Utility class for providing tools to use in the Vigenère cipher breaker program.
+ *
+ * This class provides static methods for formatting the key string, decrypting ciphertext strings,
+ * setting the key length, and getting the key length.
+ */
 class vigenereCipher {
 public:
+   /**
+    * Formats the key string so that it is at least as long as the ciphertext string.
+    *
+    * @param ciphertext The ciphertext string.
+    * @param key The key string.
+    * @return The formatted key string.
+    */
     static std::string formatKey(const std::string &ciphertext, const std::string &key);
 
+   /**
+    * Decrypts a ciphertext string using the Vigenère cipher with the given key.
+    *
+    * @param ciphertext The ciphertext string to decrypt.
+    * @param key The key to use for decryption.
+    * @return The decrypted plaintext string.
+    */
     static std::string decrypt(std::string ciphertext, std::string key);
 
+   /**
+    * Sets the length of the key used for encryption and decryption.
+    *
+    * @param length The length of the key.
+    */
     static void setKeyLength(int length);
 
+   /**
+    * Gets the length of the key used for encryption and decryption.
+    *
+    * @return The length of the key.
+    */
     static int getKeyLength();
 
 private:
+   /**
+    * The length of the key used for encryption and decryption.
+    */
     inline static int keyLength;
 };
 
